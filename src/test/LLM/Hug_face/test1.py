4addb42f9639e17ab6ce5a3d01010900b4d2b6ad
Dilip kumar <dilipkumarg77@gmail.com>
2025-06-05 07:12:14 +0530

    updated test1.py (#1)

--- src/test/LLM/Hug_face/test1.py
+++ src/test/LLM/Hug_face/test1.py
@@ -2,6 +2,6 @@ from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 
-res = classifier(" I love to code in python with pytorch")
+res = classifier("I have no strong feelings about the movie")
 
 print (res)
